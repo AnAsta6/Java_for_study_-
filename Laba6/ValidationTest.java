cd56ded0d0e2a76d6b6c5cbf4430d6eb61bab019
AnAsta6 <145312260+AnAsta6@users.noreply.github.com>
2024-12-24 08:16:20 +0500

    Update ValidationTest.java

--- Laba6/ValidationTest.java
+++ Laba6/ValidationTest.java
@@ -5,7 +5,7 @@ public class ValidationTest {
 
     @Test
     //возраст человека в диапазоне от 1 до 200 корректен
-    public void testHumanVValidAge() {
+    public void test1() {
         HumanV human = new HumanV(50);
         assertTrue(HumanTestsV.testValidAge(human));
         assertTrue(HumanTestsV.testAgeRange(human));
@@ -13,7 +13,7 @@ public class ValidationTest {
 
     @Test
     // 0 некорректно
-    public void testHumanVInvalidAge() {
+    public void test2() {
         HumanV human = new HumanV(0);
         assertFalse(HumanTestsV.testValidAge(human));
         assertFalse(HumanTestsV.testAgeRange(human));
@@ -21,7 +21,7 @@ public class ValidationTest {
 
     @Test
     // 250 является корректным, но не в диапазоне от 1 до 200
-    public void testHumanVOutOfRangeAge() {
+    public void test3() {
         HumanV human = new HumanV(250);
         assertTrue(HumanTestsV.testValidAge(human));
         assertFalse(HumanTestsV.testAgeRange(human));
@@ -29,7 +29,7 @@ public class ValidationTest {
 
     @Test
     //от 1 до 150 корректно
-    public void testPersonValidAge() {
+    public void test4() {
         Person person = new Person(50);
         assertTrue(PersonTests.testValidAge(person));
         assertTrue(PersonTests.testAgeRange(person));
@@ -39,7 +39,7 @@ public class ValidationTest {
 
     @Test
     // 0 некорректно для персоны
-    public void testPersonInvalidAge() {
+    public void test5() {
         Person person = new Person(0);
         assertFalse(PersonTests.testValidAge(person));
         assertFalse(PersonTests.testAgeRange(person));
@@ -47,7 +47,7 @@ public class ValidationTest {
 
     @Test
     //160 корректно, но не в [1;150]
-    public void testPersonOutOfRangeAge() {
+    public void test6() {
         Person person = new Person(160);
         assertTrue(PersonTests.testValidAge(person));
         assertFalse(PersonTests.testAgeRange(person));
@@ -55,15 +55,16 @@ public class ValidationTest {
 
     //валидатор убирает исключение для неправильного возраста
     @Test(expected = ValidatorV.ValidationExceptionV.class)
-    public void testValidatorVException() throws Exception {
+    public void test7() throws Exception {
         HumanV human = new HumanV(250);
         ValidatorV.validate(human);
     }
 
     // тоже самое, но не убирает для правильного
     @Test
-    public void testValidatorVNoException() throws Exception {
+    public void test8() throws Exception {
         HumanV human = new HumanV(50);
         ValidatorV.validate(human);
     }
 }
+
